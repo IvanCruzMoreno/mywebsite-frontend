02850a5c9f218350b1b07d5d252e902cfde5a433
ivan <ivanmoreno405@gmail.com>
2021-07-07 11:23:46 -0500

    organizing the components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FrontPageComponent } from './components/front-page.component';
 import { ReposComponent } from './components/repos.component';
 
 const routes: Routes = [
+  {path: '', pathMatch: 'full', redirectTo: 'intro'},
+  {path: 'intro', component: FrontPageComponent},
   {path: 'repos', component: ReposComponent}
 ];
 
